ffca6563164766764cf717df7ac0f10e58bf4cbc
Matthias Urlichs <matthias@urlichs.de>
2024-12-13 05:39:43 +0100

    Don't include version and author in __init__
    That slows down package loading. A lot.
    Callers should use importlib to check the version if they need it.

--- aionotify/__init__.py
+++ aionotify/__init__.py
@@ -1,13 +1,8 @@
 # Copyright (c) 2016 The aionotify project
 # This code is distributed under the two-clause BSD License.
 
-from importlib.metadata import version
-
 from .enums import Flags
 from .base import Watcher
 
 __all__ = ['Flags', 'Watcher']
 
-
-__version__ = version("aionotify")
-__author__ = 'Raphaël Barrois <raphael.barrois+aionotify@polytechnique.org>'
